f2be93e5ce6a8526bf8a4d6de75a1f9551b8b236
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2023-09-26 19:32:33 +0300

    bgf

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,17 +3,40 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const ulEl = document.querySelector('.gallery');
+ulEl.addEventListener('click', handleClick);
+
+function galleryImg(arr) {
+    return arr
+    .map(
+        ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+         <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+         />
+        </a>
+       </li>`
+    )
+    .join("");
+}
 
 
 
 
 
 
-console.log(_.throttle);
+
 
 
 {/* <li class="gallery__item">
    <a class="gallery__link" href="large-image.jpg">
-      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+      <img 
+      class="gallery__image" 
+      src="small-image.jpg" 
+      alt="Image description" />
    </a>
 </li> */}
