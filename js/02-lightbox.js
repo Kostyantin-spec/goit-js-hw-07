2f16c92e465e3a9f7f2f8babfe4da7f2dc7edd07
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2023-09-26 20:35:20 +0300

    тпо

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,7 +15,7 @@ function galleryImg(arr) {
          <img
            class="gallery__image"
            src="${preview}"
-           data-source="${original}"
+          
            alt="${description}"
          />
         </a>
@@ -23,22 +23,33 @@ function galleryImg(arr) {
     )
     .join("");
 }
-// function handleClick(event) {
-//     event.preventDefault();
-//     if(event.target === event.currentTarget){ 
-//     return;
-// }
-
-//const img = event.target.closest(".gallery__image");
-//console.log(img);
 
 
+// Автономный плагин
+// При использовании автономного варианта (`simple-lightbox(.min).js`)
 
+// var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+// При использовании автономного варианта (`simple-lightbox(.min).js`)
 
+// var lightbox = $('.gallery a').simpleLightbox({ /* options */ });
+// С помощью Webpack/Browserify/Parcel и т. д.
+// Выберите файл модуля, который вы хотите импортировать или потребовать.
 
+// Модуль с трансформацией Babel
+// import SimpleLightbox from "simplelightbox";
 
+// Обычный модуль ES без Babel
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+// Разметка
+// Для настройки по умолчанию вам просто нужны ссылки, указывающие на изображения.
 
+// <div class="gallery">
+//     <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
+//     <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
+// </div>
+// Разметка внутри A-тегов может быть любой. В этом примере миниатюры больших изображений. Тег заголовка по умолчанию используется для отображения подписи. Для полного примера просто посмотрите папку demo.
 
+// Разметка
 {/* <li class="gallery__item">
    <a class="gallery__link" href="large-image.jpg">
       <img 
@@ -47,3 +58,19 @@ function galleryImg(arr) {
       alt="Image description" />
    </a>
 </li> */}
+
+//примері
+// let gallery = new SimpleLightbox('.gallery a');
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
+
+// gallery.on('error.simplelightbox', function (e) {
+// 	console.log(e); // some usefull information
+// });
+
+// // with jQuery nearly the same
+// let gallery = $('.gallery a').simpleLightbox();
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
